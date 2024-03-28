e5d43883c829e7332c025dd4a04fbe7a0e9c219f
Lizzie <lizziejacoboo@gmail.com>
2024-03-27 20:05:46 -0600

    schemas created

--- server/server.js
+++ server/server.js
@@ -1,25 +1,51 @@
 const express = require('express');
+const { ApolloServer } = require('apollo-server-express');
+const { typeDefs, resolvers } = require('./schemas');
+
 const path = require('path');
 const db = require('./config/connection');
-const routes = require('./routes');
+// const routes = require('./routes');
+const { authMiddleware } = require('./utils/auth');
 
-const app = express();
+// console.log('__dirname resolves to: ', __dirname);
+// const { typeDefs, resolvers } = require('./schemas');
+
 const PORT = process.env.PORT || 3001;
+const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// if we're in production, serve client/build as static assets
 
-app.use(express.static(path.join(__dirname, '../client/dist')));
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/dist')));
+}
 
-
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname + '/client/dist/index.html'));
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: ({ req }) => authMiddleware({ req }),
 });
 
-app.use(routes);
+// Start the Apollo server before applying middleware to the Express app
+async function startServer() {
+  await server.start();
+  server.applyMiddleware({ app });
 
-db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+  app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '..', 'client', 'dist', 'index.html'));
 });
+
+  // app.use(routes);
+
+  db.once('open', () => {
+    app.listen(PORT, () =>
+      console.log(`🌍 Now listening on localhost:${PORT}`)
+    );
+    console.log(
+      `🚀 GraphQL ready at http://localhost:${PORT}${server.graphqlPath}`
+    );
+  });
+}
+
+startServer();
